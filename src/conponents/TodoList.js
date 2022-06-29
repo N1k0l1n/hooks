f14d63a968de7b8fa1cefd68c5f6a43239963a72
N1k0L1n <nikolinandoni80@gmail.com>
2022-06-29 16:06:54 +0200

    first commit

---
+++ src/conponents/TodoList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {ThemeContext} from "../context/ThemeContext"
+
+class TodoList extends React.Component{
+    static contextType=ThemeContext;
+    render(){
+        const {isDarkTheme, lightTheme, darkTheme, changeTheme } = this.context;
+        const theme = isDarkTheme ? darkTheme : lightTheme;
+        return(
+            <div style ={{background:theme.background, color:theme.text, height:'140px', textAlign:"center"}}className="ui list">
+                <p className="item">Plan the family trip</p>
+                <p className="item">Go for shopping for dinner</p>
+                <p className="item">Go for a walk</p>
+                <button className="ui botton primary" onClick={changeTheme}>Change the Theme</button>
+            </div>
+        )
+    }
+}
+export default TodoList;
